06a50b4149829d26144c974f0037f98a2e3e5134
silvermaking <silvermaking@gmail.com>
2022-01-11 20:09:42 +0900

    java-spring

---
+++ hello-srping/src/main/java/hello/hellosrping/HelloSrpingApplication.java
@@ -0,0 +1,13 @@
+package hello.hellosrping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSrpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSrpingApplication.class, args);
+	}
+
+}
---
+++ hello-srping/src/test/java/hello/hellosrping/HelloSrpingApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellosrping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSrpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
