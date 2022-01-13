36da85817fadef72c4df6109c77d3b5317712857
silvermaking <silvermaking@gmail.com>
2022-01-13 21:57:09 +0900

    9강

--- hello-srping/src/main/java/hello/hellosrping/controller/HelloController.java
+++ hello-srping/src/main/java/hello/hellosrping/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,25 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //문자 그대로 출력, 태그도 없이
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name; //"hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+
+        
+        }
     }
 }
